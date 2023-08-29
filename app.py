43920cf00bdd9e00bdc24932dfef4806075328f5
jinrumiao <122008339+jinrumiao@users.noreply.github.com>
2023-08-29 20:23:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,39 @@ line_bot_api = LineBotApi('FodjM2t1CPtpwbASYaro4QZ1imRri8LnN7x9E9JHS0I6mD8IBDhHf
 handler = WebhookHandler('1dbcb5c802532d8a5455e44d206af583')
 
 
+def about_us_event(event):
+    emoji = [
+        {
+            "index": 0,
+            "productId": "5ac21184040ab15980c9b43a",
+            "emojiId": "225"
+        },
+        {
+            "index": 17,
+            "productId": "5ac21184040ab15980c9b43a",
+            "emojiId": "225"
+        }
+    ]
+
+    text_message = TextSendMessage(text='''$ Master RenderP $
+Hello! 您好，歡迎您成為 Master RenderP 的好友！
+
+我是Master 支付小幫手 
+
+-這裡有商城，還可以購物喔~
+-直接點選下方【圖中】選單功能
+
+-期待您的光臨！''', emojis=emoji)
+
+    sticker_message = StickerSendMessage(
+        package_id='8522',
+        sticker_id='16581271'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
+
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,9 +71,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # event有什麼資料？詳見補充
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id
+    message_text = str(event.message.text).lower()
 
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text='Hi! Welcome to LSTORE.'))
+    if message_text == "@使用說明":
+        about_us_event(event)
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hi! Welcome to LSTORE.'))
 
 
 if __name__ == "__main__":
